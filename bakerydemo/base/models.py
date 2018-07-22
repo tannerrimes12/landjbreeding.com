2477781e2210750599c1743ee29b15870f5b8048
Tanner Grimes <tannergrimes12@gmail.com>
2018-07-22 17:25:26 -0500

    Horselist blocks

--- bakerydemo/base/models.py
+++ bakerydemo/base/models.py
@@ -38,6 +38,11 @@ class Horse(models.Model):
     ), default = 'M')
     breed = models.ForeignKey('Breed', models.SET_DEFAULT, default = Breed.objects.get(name='Unknown').pk)
     # breed = models.ForeignKey('Breed', models.SET_NULL, null = True, blank = True)
+    status = models.CharField(max_length = 3, choices = (
+        ('FS', 'For Sale'),
+        ('S', 'Sold'),
+        ('NFS', 'Not For Sale'),
+    ), default = 'NFS')
 
 
     panels = [
@@ -45,91 +50,11 @@ class Horse(models.Model):
         FieldPanel('description'),
         FieldPanel('breed'),
         FieldPanel('sex'),
+        FieldPanel('status'),
 
     ]
 
 
-
-
-
-# @register_snippet
-# class People(index.Indexed, ClusterableModel):
-#     """
-#     A Django model to store People objects.
-#     It uses the `@register_snippet` decorator to allow it to be accessible
-#     via the Snippets UI (e.g. /admin/snippets/base/people/)
-#
-#     `People` uses the `ClusterableModel`, which allows the relationship with
-#     another model to be stored locally to the 'parent' model (e.g. a PageModel)
-#     until the parent is explicitly saved. This allows the editor to use the
-#     'Preview' button, to preview the content, without saving the relationships
-#     to the database.
-#     https://github.com/wagtail/django-modelcluster
-#     """
-#     first_name = models.CharField("First name", max_length=254)
-#     last_name = models.CharField("Last name", max_length=254)
-#     job_title = models.CharField("Job title", max_length=254)
-#
-#     image = models.ForeignKey(
-#         'wagtailimages.Image',
-#         null=True,
-#         blank=True,
-#         on_delete=models.SET_NULL,
-#         related_name='+'
-#     )
-#
-#     panels = [
-#         MultiFieldPanel([
-#             FieldRowPanel([
-#                 FieldPanel('first_name', classname="col6"),
-#                 FieldPanel('last_name', classname="col6"),
-#             ])
-#         ], "Name"),
-#         FieldPanel('job_title'),
-#         ImageChooserPanel('image')
-#     ]
-#
-#     search_fields = [
-#         index.SearchField('first_name'),
-#         index.SearchField('last_name'),
-#     ]
-#
-#     @property
-#     def thumb_image(self):
-#         # Returns an empty string if there is no profile pic or the rendition
-#         # file can't be found.
-#         try:
-#             return self.image.get_rendition('fill-50x50').img_tag()
-#         except:
-#             return ''
-#
-#     def __str__(self):
-#         return '{} {}'.format(self.first_name, self.last_name)
-#
-#     class Meta:
-#         verbose_name = 'Person'
-#         verbose_name_plural = 'People'
-
-
-# @register_snippet
-# class FooterText(models.Model):
-#     """
-#     This provides editable text for the site footer. Again it uses the decorator
-#     `register_snippet` to allow it to be accessible via the admin. It is made
-#     accessible on the template via a template tag defined in base/templatetags/
-#     navigation_tags.py
-#     """
-#     body = RichTextField()
-#
-#     panels = [
-#         FieldPanel('body'),
-#     ]
-#
-#     def __str__(self):
-#         return "Footer text"
-#
-#     class Meta:
-#         verbose_name_plural = 'Footer Text'
 
 
 class StandardPage(Page):
---
+++ bakerydemo/base/templatetags/custom_tags.py
@@ -0,0 +1,25 @@
+from django import template
+
+from wagtail.images.models import Image
+
+from bakerydemo.base.models import Horse
+
+register = template.Library()
+
+@register.simple_tag()
+def get_horses(horse_type):
+
+    horse_list = None
+
+    if horse_type == 'stallion':
+        horse_list = Horse.objects.filter(sex='M', status = 'NFS')
+    elif horse_type == 'mare':
+        horse_list = Horse.objects.filter(sex='F', status = 'NFS')
+    elif horse_type == 'forsale':
+        horse_list = Horse.objects.filter(status='FS')
+    elif horse_type == 'sale':
+        horse_list = Horse.objects.filter(status='S')
+
+
+
+    return horse_list
