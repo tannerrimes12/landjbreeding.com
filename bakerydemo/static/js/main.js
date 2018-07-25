57a84db40733c7bc57450783463c94f533757d1c
Tanner Grimes <tannergrimes12@gmail.com>
2018-07-25 17:09:20 -0500

    Preventing download image

--- bakerydemo/static/js/main.js
+++ bakerydemo/static/js/main.js
@@ -1,12 +1,21 @@
 $(function(){
   console.log('attaching fancybox');
-  $("a.gallery-image").fancybox();
+  $("a.gallery-image").fancybox({
+    protect: true,
+    buttons : [
+      'zoom',
+      'thumbs',
+      'close'
+    ]
+  });
+
+  $('.thumbnail-list').bind("contextmenu",function(e) {
+    return false;
+  });
 });
 
-$(document).ready(function()
-{
-    $(document).bind('contextmenu', function()
-    {
-        return false;
-    });
-});
+// $(document).ready(function() {
+//   $('.thumbnail-list').bind("contextmenu",function(e) {
+//     return false;
+//   });
+// });
